8f7d1d8b8aa0b19852a1ecabb6f6be930c06a34c
user <user@LENOVOL15G1-15>
2024-01-09 20:42:53 +0100

    Add project files.

---
+++ gyfel_030/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//B.30.feladat: Készítsünk  programot,  amely bekéri  a  víz  hőmérsékletét, majd  eldönti, hogy  az  milyen halmazállapotú. A halmazállapot lehet folyékony, gőz, vagy jég.
+
+namespace gyfel_030
+{
+    class Program
+    {
+        static void Main()
+        {
+            int ertek = int.Parse(Console.ReadLine());
+            VizHalmazallapota(ertek);
+            Console.ReadLine();
+        }
+
+        static void VizHalmazallapota(int szam)
+        {
+            if (szam >= 100)
+                Console.WriteLine($"A {szam} fokos vizet gőznek hívjuk!");
+            else if (szam < 0)
+                Console.WriteLine($"A {szam} fokos vizet jégnek hívjuk!");
+            else
+                Console.WriteLine($"A {szam} fokos víz folyékony halmazállapotú.");
+
+
+        }
+    }
+}
